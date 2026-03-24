7a88a876b2c04574df6c733612f5a56dbfcd50f9
Lucien Peng <lucien.peng@og1o.tw>
2026-03-24 12:08:33 +0800

    fix: add_entry fns

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import json
 from repository import add_entry
 
 
@@ -8,15 +7,9 @@ def main():
 
     data = {"entries": []}
 
-    data = add_entry(
-        data, title="Gmail", username="user@gmail.com", password="p@ssw0rd"
-    )
-    data = add_entry(data, title="GitHub", username="devuser", password="gh_token_123")
+    add_entry(data, title="Gmail", username="user@gmail.com", password="p@ssw0rd")
 
-    with open("vault.json", "w", encoding="utf-8") as f:
-        json.dump(data, f, indent=2, ensure_ascii=False)
-
-    print("Save to vault.json")
+    add_entry(data, title="GitHub", username="devuser", password="gh_token_123")
 
 
 if __name__ == "__main__":

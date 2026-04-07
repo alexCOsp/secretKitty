70de38f4c44055b67a8da52af5a6a4e4db24e8ca
Lucien Peng <lucien.peng@og1o.tw>
2026-04-07 10:43:27 +0800

    fix: read and write fns

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 from repository import add_entry
 from repository import get_all_entries
+from repository import get_entry_by_id
 
 
 def main():
 
     print("Hello from Secret kitty!")
 
-    data = {"entries": []}
+    add_entry(
+        title="Gmail",
+        username="user@gmail.com",
+        password="p@ssw0rd",
+        notes="Personal email account",
+        url="https://mail.google.com",
+    )
 
-    add_entry(data, title="Gmail", username="user@gmail.com", password="p@ssw0rd", notes="Personal email account", url="https://mail.google.com")
+    add_entry(
+        title="GitHub",
+        username="devuser",
+        password="gh_token_123",
+        notes="Work projects",
+        url="https://github.com",
+    )
 
-    add_entry(data, title="GitHub", username="devuser", password="gh_token_123", notes="Work projects", url="https://github.com")
+    get_all_entries()
 
-    get_all_entries(data)
+    get_entry_by_id("d290f1ee-6c54-4b01-90e6-d701748f0851")
 
 
 if __name__ == "__main__":
--- repository.py
+++ repository.py
@@ -4,8 +4,9 @@ from datetime import datetime
 
 
 # ── CREATE ──────────────────────────────
-def add_entry(data: dict, title, username, password, notes: str, url: str):
-    entry = {
+def add_entry(title: str, username: str, password: str, notes: str, url: str) -> None:
+
+    newEntry = {
         "id": str(uuid.uuid4()),
         "title": title,
         "username": username,
@@ -15,29 +16,41 @@ def add_entry(data: dict, title, username, password, notes: str, url: str):
         "created_at": datetime.now().isoformat(),
         "updated_at": datetime.now().isoformat(),
     }
-    data["entries"].append(entry)
 
+    # Read existing data
+    with open("vault.json", "r", encoding="utf-8") as f:
+        currentData: dict[str, list] = json.load(f)
+
+    # Add new entry
+    currentData["entries"].append(newEntry)
+
+    # Write updated data back to file
     with open("vault.json", "w", encoding="utf-8") as f:
-        json.dump(data, f, indent=2, ensure_ascii=False)
+        json.dump(currentData, f, indent=2, ensure_ascii=False)
 
     print("Save to vault.json")
 
 
 # ── READ（All）────────────────────────
-def get_all_entries(data: dict) -> list:
+def get_all_entries() -> list:
+
+    with open("vault.json", "r", encoding="utf-8") as f:
+        data = json.load(f)
+
     print(sorted(data["entries"], key=lambda e: e["title"]))
     return sorted(data["entries"], key=lambda e: e["title"])
 
 
 # ── READ（SINGLE）────────────────────────
-def get_entry_by_id(data: dict, id: str) -> dict | None:
+def get_entry_by_id(id: str) -> dict | None:
+    with open("vault.json", "r", encoding="utf-8") as f:
+        data = json.load(f)
+
     for entry in data["entries"]:
         if entry["id"] == id:
             print(entry)
             return entry
-        else:
-            print(f"Entry with id {id} not found.")
-            return None
+    print(f"Entry with id {id} not found.")
     return None
 
 
